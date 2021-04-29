d174ba5ed639bccc8363ce422b89d0e21d2f93e1
[crsgustav0] <[cristian.dias@terraviva.agr.br]>
2021-04-29 15:42:40 -0300

    Atualização .gitnore

---
+++ src/Constantes.java
@@ -0,0 +1,50 @@
+package br.agr.terraviva.modelogestao.util;
+
+public class Constantes {
+    //BANCO DE DADOS
+    public static final String BANCO = "MEGTV55UGB_11.db3";
+    public static final int VERSAO = 11;
+
+    //USUARIO
+    public static final String WS_USER = "tv_mobile_user";
+    public static final String WS_PSW = "tv_mobile_psw";
+
+    //WS EM COMUM
+    public static final String PROCESSO = "000003";//Proc. Modelo de Gestao
+
+    public static final String WS_USUARIO = "ZGTIW004";//Usuario
+    public static final String WS_GRAVAR_SENHA = "ZGTIW005";//Alterar Senha
+    public static final String WS_FILIAL = "ZGTIW001";//Filial
+    public static final String WS_LOCAL = "ZTRMW003";//Local
+    public static final String WS_COMPETENCIA = "ZTRMW001";//Competencia
+    public static final String WS_ENVIO_FOTOS = "ZTRMW013";//Gravação / Envio Fotos
+    public static final String WS_BUSCA_FOTOS = "ZTRMW017";//Alteração / Busca Fotos
+
+    //AUDITORIA UGB
+    public static final String SUBPROCESSO_1 = "000001";//Auditoria UGB
+
+    public static final String WS_AUDITORIA_UGB = "ZTRMW019"; //Gravação
+    public static final String WS_OBT_AUDITORIA_UGB = "ZTRMW018";//Alteração
+    public static final String WS_UGB = "ZTRMW005";//UGB
+    public static final String WS_SUPERIOR = "ZTRMW004";//Superior
+    public static final String WS_COLABORADOR = "ZTRMW006";//Colaborador
+    public static final String WS_PESQXPERG_UGB = "ZTRMW018";//Pesquisa UGB
+
+    //AUDITORIA 5S
+    public static final String SUBPROCESSO_2 = "000002";//AUDITORIA 5S
+
+    public static final String WS_AUDITORIA_5S = "ZTRMW011";//Gravação
+    public static final String WS_OBT_AUDITORIA_5S = "ZTRMW012"; //Alteração
+    public static final String WS_PESQXPERG_5S = "ZTRMW010";//Pesquisa 5S
+    public static final String WS_AMBIENTE = "ZTRMW008";//Ambiente
+    public static final String WS_COLABORADOR_5S = "ZGTIW002";//Colaborador 5S
+
+    //Armazenamento de Arquivos
+    public static final String CAMINHO_GALERIA_ANDROID_10 = "/storage/emulated/0/Pictures/aud5s/";
+    public static final String CAMINHO_GALERIA_ANDROID_9 = "/storage/emulated/0/aud5s/";
+
+    //Titulos
+    public static final String TITULO_ACTIVITY_UGB = "Auditoria UGB";
+    public static final String TITULO_ACTIVITY_5S = "Auditoria 5S";
+
+}
